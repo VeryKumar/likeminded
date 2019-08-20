6be00940d167a735cc2b6a0f5f1e112594aa6128
Jason Shockness <jshock12@gmail.com>
2019-08-20 13:40:45 -0400

    tues 140

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :user 
-resources :group, only: [:index, :show]
+resources :users
+resources :groups, only: [:index, :show]
 resources :tests, only: [:show]
 end
