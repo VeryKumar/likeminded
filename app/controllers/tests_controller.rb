ee8bf101e1982a56eed984638d4f3ba58666eb45
Varun Kumar <varunkumaranalysis@gmail.com>
2019-08-20 16:40:06 -0400

    started test views

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,6 @@
 class TestsController < ApplicationController
     def index
-        @tests = Tests.all
+        @tests = Test.all
     end
 
     def new
@@ -17,11 +17,12 @@ class TestsController < ApplicationController
     def delete
         Test.find(params[:id]).destroy
         flash[:success] = "Test Deleted"
-        redirect_to 
+        redirect_to user_path(@user)
     end
 
     private
 
     def test_params
         params.require(:test).permit(:user_id, :group_id, :e_value, :a_value, :c_value, :n_value, :o_value)
+    end
 end
