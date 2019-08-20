0a20019d753910a449209147ff2ca4d5e47ca299
Jason Shockness <jshock12@gmail.com>
2019-08-20 12:14:05 -0400

    user&group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+
+    def index
+        @groups = Group.all 
+    end
+
+    def show
+        @group = Group.find(params[:id])
+        @users = User.all
+    end
+
+    private
+
+    def group_params
+        params.require(@group).permit(:name)
 end
